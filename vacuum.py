d425fb6d8333ab373f9dd2389f9d8e405697b4e9
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-05-10 10:21:42 +0500

    Update vacuum.py

--- vacuum.py
+++ vacuum.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 
 class Environment(object):
     def __init__(self):
@@ -8,44 +8,44 @@ class Environment(object):
         self.locationCondition['B'] = random.randint(0, 1)
 
 
-class SimpleReflexVacuumAgent(Environment):
+class SimpleReflexsprayAgent(Environment):
     def __init__(self, Environment):
         print (Environment.locationCondition)
         Score = 0
-        vacuumLocation = random.randint(0, 1)
-        if vacuumLocation == 0:
-            print ("Vacuum is randomly placed at Location A.")
-            print ("Location A is Dirty.") if Environment.locationCondition['A'] == 1 else print("Location A is Clean.")
+        sprayLocation = random.randint(0, 1)
+        if sprayLocation == 0:
+            print ("spray is randomly placed at Location A.")
+            print ("Location A is INFECTED.") if Environment.locationCondition['A'] == 1 else print("Location A is DISINFECTED.")
             if Environment.locationCondition['A'] == 1:
                 Environment.locationCondition['A'] = 0
                 Score += 1
-                print ("Location A has been Cleaned.") 
+                print ("Location A has been DISINFECTED.") 
             print ("Moving to Location B...")
-            print ("Location B is Dirty.") if Environment.locationCondition['B'] == 1 else print("Location B is Clean.")
+            print ("Location B is INFECTED.") if Environment.locationCondition['B'] == 1 else print("Location B is DISINFECTED.")
             if Environment.locationCondition['B'] == 1:
                 Environment.locationCondition['B'] = 0;
                 Score += 1
-                print ("Location B has been Cleaned.")
-            print("Environment is Clean.")
+                print ("Location B has been DISINFECTED.")
+            print("Environment is DISINFECTED.")
                 
-        elif vacuumLocation == 1:
-            print ("Vacuum randomly placed at Location B.")
-            print ("Location B is Dirty.") if Environment.locationCondition['B'] == 1 else print("Location B is Clean.")
+        elif sprayLocation == 1:
+            print ("spray randomly placed at Location B.")
+            print ("Location B is INFECTED.") if Environment.locationCondition['B'] == 1 else print("Location B is DISINFECTED.")
             if Environment.locationCondition['B'] == 1:
                 Environment.locationCondition['B'] = 0
                 Score += 1
-                print ("Location B has been Cleaned.")
+                print ("Location B has been DISINFECTED.")
             print ("Moving to Location A...")
-            print ("Location A is Dirty.") if Environment.locationCondition['A'] == 1 else print("Location A is Clean.")
+            print ("Location A is INFECTED.") if Environment.locationCondition['A'] == 1 else print("Location A is DISINFECTED.")
             if Environment.locationCondition['A'] == 1:
                 Environment.locationCondition['A'] = 0;
                 Score += 1
-                print ("Location A has been Cleaned.")
-            print("Environment is Clean.")    
+                print ("Location A has been DISINFECTED.")
+            print("Environment is DISINFECTED.")    
             
         print (Environment.locationCondition)
         print ("Performance Measurement: " + str(Score))
 
 
 theEnvironment = Environment()
-theVacuum = SimpleReflexVacuumAgent(theEnvironment)
+thespray = SimpleReflexsprayAgent(theEnvironment)
