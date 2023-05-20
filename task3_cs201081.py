ac7d6127dbc6a1970444e24998ef8a217a46b984
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-05-20 10:57:29 +0500

    Create task3_cs201081.py

---
+++ task3_cs201081.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+
+def dfs_vacuum_cleaner(room, visited):
+    print("Cleaning room #", room)
+
+    visited[room] = True
+
+    # Generate random neighboring rooms
+    neighbors = random.sample(range(20), 4)
+
+    for neighbor in neighbors:
+        if not visited[neighbor]:
+            dfs_vacuum_cleaner(neighbor, visited)
+            break
+    else:
+        print("Backtracking from room", room)
+
+def main():
+    # Initialize rooms as dirty
+    rooms = [True] * 20
+    print("Vacuum to be placed in any of the 20 rooms")
+
+    # Choose a random starting room
+    start_room = random.randint(0, 19)
+    
+    print("Starting room:", start_room)
+
+    # Mark the starting room as visited
+    visited = [False] * 20
+    visited[start_room] = True
+
+    # Clean the rooms using DFS
+    dfs_vacuum_cleaner(start_room, visited)
+
+if __name__ == '__main__':
+    main()
