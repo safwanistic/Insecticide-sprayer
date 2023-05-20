4ef17183276d074608edd4075c3ef172bd217bf3
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-05-20 18:12:56 +0500

    Update task6_cs201081.py

--- task6_cs201081.py
+++ task6_cs201081.py
@@ -1,18 +1,14 @@
 class K_MST:
     
-    #Creates two lists, parent and rank, to keep track of the parent and rank of each element in the set.
     def __init__(self, n):
         self.parent = list(range(n))
         self.rank = [0] * n
 
-    #Recursively finds the representative (root) of the set to which x belongs    
     def find(self, x):
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
  
-    #Merges the sets to which x and y belong by making the root of one set the parent of the other. 
-    #The rank is used to optimize the union operation to maintain a balanced tree structure.
     def un(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
@@ -25,7 +21,7 @@ class K_MST:
                 self.parent[root_y] = root_x
                 self.rank[root_x] += 1
 
-#Function for performing the Kruskal Algo
+
 def kruskal_mst(graph):
     n = len(graph)
     edges = []
@@ -33,7 +29,7 @@ def kruskal_mst(graph):
         for j in range(i+1, n):
             if graph[i][j] != 0:
                 edges.append((i, j, graph[i][j]))
-    edges.sort(key=lambda x: x[2])  #Sorting edges by weight
+    edges.sort(key=lambda x: x[2]) 
     mst = []
     uf = K_MST(n)
     for edge in edges:
@@ -44,11 +40,11 @@ def kruskal_mst(graph):
     return mst
 
 graph = [
-    [0, 2, 0, 6, 3],
+    [0, 5, 0, 6, 3],
     [2, 0, 3, 8, 5],
-    [0, 6, 0, 0, 7],
-    [6, 8, 0, 0, 7],
-    [0, 5, 4, 9, 0]
+    [4, 6, 1, 0, 7],
+    [6, 3, 0, 4, 5],
+    [0, 2, 4, 9, 0]
 ]
 
 mst = kruskal_mst(graph)
