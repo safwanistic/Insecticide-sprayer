98c5def41096ea79dec3acf6ea0256a084aa51f0
Muhammad Ashhad Safwan <m_ashhadhlr@yahoo.com>
2023-05-20 11:08:54 +0500

    Removing extra comments

--- task3_cs201081.py
+++ task3_cs201081.py
@@ -8,7 +8,6 @@ def dfs_vacuum_cleaner(room, visitedRooms):
 
     visitedRooms[room] = True
 
-    # Generate random neighboring rooms
     neighbors = random.sample(range(20), 4)
 
     for neighbor in neighbors:
@@ -21,20 +20,19 @@ def dfs_vacuum_cleaner(room, visitedRooms):
         time.sleep(.95)
 
 def main():
-    # Initialize rooms as dirty
     rooms = [True] * 20
     print("Vacuum to be placed in any of the 20 rooms")
     time.sleep(.85)
-    # Choose a random starting room
+
     start_room = random.randint(0, 19)
+
     time.sleep(.85)
+    
     print("Starting room: ", start_room)
 
-    # Mark the starting room as visitedRooms
     visitedRooms = [False] * 20
     visitedRooms[start_room] = True
 
-    # Clean the rooms using DFS
     dfs_vacuum_cleaner(start_room, visitedRooms)
 
 if __name__ == '__main__':
